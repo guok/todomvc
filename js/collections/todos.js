b45573d0739d2802b31dada6f415857f58468ffc
Kenny Guo <kenny.guo@autodesk.com>
2015-04-10 16:40:21 +0800

    Firstly it can work basically, and without any js error.

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -4,7 +4,7 @@ var app = app || {};
 var TodoList = Backbone.Collection.extend({
 	model: app.Todo,
 	localStorage: new Backbone.LocalStorage("todos-backbone"),
-	completed:functon(){
+	completed:function(){
 		return this.filter(function(todo){
 			return todo.get("completed");
 		});
